a76cec74142ec10911efb4e1b71416d92861f793
Ryan <ryan@ryansolomon.io>
2023-08-19 09:39:12 -0600

    rebuild

--- app/api/add-post/route.js
+++ app/api/add-post/route.js
@@ -4,8 +4,6 @@ import { NextResponse } from "next/server"
 export async function POST(request){
     const res = await request.json()
     const {title, content} = res;
-    console.log({res})
-
      const result = await prisma.post.create({
         data: {
             title,
